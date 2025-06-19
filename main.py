b2bb5ccdf621755f42766ecc597b02de6464b1d0
Victor-Diniz-Dev <victordinizdev@gmail.com>
2025-06-19 01:17:16 -0300

    criado o primeiro esboço e tamanho fixo da janela

---
+++ main.py
@@ -0,0 +1,21 @@
+from tkinter import *
+
+root = Tk()
+
+root.title('Calculadora')
+root.geometry('408x355')
+root.minsize(408,355)
+root.maxsize(408, 355)
+root.configure(background='#282828')
+
+e = Entry(root, width=15, borderwidth=4, relief=FLAT, fg='#FFFFFF', bg='#A7A28F', font=('futura', 25, 'bold'), justify=CENTER)
+e.grid(
+    row=0,
+    column=0,
+    columnspan=4,
+    pady=2
+)
+
+#
+
+root.mainloop()
